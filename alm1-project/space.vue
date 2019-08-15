3009874cb8d9da1d328630af965da164b008f5d4
糖小米 <tangxiaomiemail@gmail.com>
2019-08-15 21:01:07 +0800

    charset utf -8

---
+++ alm1-project/space.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        im - template
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'space',
+    components: {},
+    computed: {},
+    data () {
+        return {};
+    },
+    created () {},
+    methods: {}
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "../theme/variables.sty"
+</style>
